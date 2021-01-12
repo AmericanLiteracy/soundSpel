f1b1b577810c948c16e9ce4557ea9969813eb0ad
Mark R. Petersen <mpetersen@lanl.gov>
2021-01-11 22:31:29 -0700

    read and write csv with pandas

--- tools/addRondthaler.py
+++ tools/addRondthaler.py
@@ -3,17 +3,16 @@ import pandas as pd
 
 fileName = 'old_versions/DIAMBG.pickle'
 fileNameIn = 'soundSpelWurdList.csv'
-fileNameIn = 'sample.csv'
+#fileNameIn = 'sample.csv'
 fileNameOut = 'addRondthalerSoundSpelWurdList.csv'
 
-#get file object
 RondthalerDict = pickle.load( open( fileName, 'rb' ) )
 df = pd.read_csv(fileNameIn, dtype=str, na_filter=False)
 ssDict = dict(zip(df.TS,df.soundSpel))
 df.insert(4, 'SSmatchRond', 'NA', allow_duplicates=True)
 
 for idx,row in df.iterrows():
-    print (idx,row['TS'])
+    #print (idx,row['TS'])
     if row['TS'] in RondthalerDict:
        df.at[idx,'Rondthaler'] = RondthalerDict[row['TS']]
        if df.at[idx,'soundSpel'] == RondthalerDict[row['TS']]:
@@ -21,36 +20,7 @@ for idx,row in df.iterrows():
        else:
            df.at[idx,'SSmatchRond'] = 'F'
     else:
-       print('did not find: ',row['TS'])
+       pass 
+       #print('did not find: ',row['TS'])
 
-
-
-
-#fileIn = open(fileNameIn, 'r')
-#fileOut = open(fileNameOut, 'w')
-#
-#iLine=1
-#while(True):
-#    #read next lineIn
-#    lineIn = fileIn.readline().rstrip('\n')
-#    if not lineIn:
-#        break
-## 0TS,1soundSpel,2note,Rondthaler,RondthalerSame
-#    field_list = lineIn.split(',')    # split the line at commas
-#    lineOut = ','.join((field_list[0],   # rejoin with commas, new order
-#                        field_list[1],
-#                        field_list[2],
-#                        field_list[3],
-#                        field_list[4],
-#                        field_list[5],
-#                        field_list[8],
-#                        field_list[9],
-#                        field_list[6],
-#                        field_list[7]
-#                           ))+'\n'
-#    # write out line
-#    fileOut.write(lineOut)
-#
-##close file
-#fileIn.close
-#fileOut.close
+df.to_csv(fileNameOut,index=False)
