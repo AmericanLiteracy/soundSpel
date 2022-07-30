5c03c2033c246f69cf853ef2399c56e381de21de
Mark Petersen <mpetersen@lanl.gov>
2022-07-30 16:32:41 -0600

    update coca untranslated

--- dictionaries/soundSpel/tools/findCocaUntranslatd.py
+++ dictionaries/soundSpel/tools/findCocaUntranslatd.py
@@ -17,7 +17,7 @@ cocaID = coca['ID']
 cocaWord = coca['w1']
 cocaRoot = coca['L1']
 # Frequency (per million words) in the 450 million word Corpus of Contemporary American English (http://corpus.byu.edu/coca)
-cocaFreqPerMil = coca['pcoca'] 
+cocaFreqPerMil = coca['coca'] 
 ssWordTO = ss['TO']
 ssWord = ss['SS']
 # using dictionary comprehension
@@ -26,11 +26,37 @@ ssDict = {ssWordTO[i]: ssWord[i] for i in range(len(ssWordTO))}
 
 cocaWordSS = []
 cocaWordNotFound = []
+cocaWordNotFoundss = []
+cocaWordNotFoundBlank = []
+cocaRootNotFound = []
+cocaIDNotFound = []
+cocaFreqNotFound = []
 for i in range(len(cocaWord)):
     try:
         cocaWordSS.append(ssDict[cocaWord[i]])
     except:
-        cocaWordSS.append('NOT_FOUND')
+        ssString = 'notfound'+str(int(round(cocaID[i]/1000)))+'k'
+        cocaWordSS.append(ssString)
+        cocaWordNotFound.append(cocaWord[i])
+        cocaWordNotFoundss.append(ssString)
+        cocaWordNotFoundBlank.append('')
+        cocaRootNotFound.append(cocaRoot[i])
+        cocaIDNotFound.append(cocaID[i])
+        cocaFreqNotFound.append(cocaFreqPerMil[i])
+
+
+df = pd.DataFrame()
+df['TO']  = cocaWordNotFound
+df['SS'] = cocaWordNotFoundBlank
+df['root']  = cocaRootNotFound
+df['freq rank']  = cocaIDNotFound
+df['freq per Mil']  = cocaFreqNotFound
+df.to_csv('workdir/coca_ordered_missing_soundSpel_20k.csv', index=False, header=True)
+
+df = pd.DataFrame()
+df['cocaWordNotFound']  = cocaWordNotFound
+df['cocaWordNotFoundss']  = cocaWordNotFoundss
+df.to_csv('workdir/coca_ordered_not_found_20k.csv', index=False)
 
 df = pd.DataFrame()
 df['cocaOrder']  = cocaID
@@ -39,4 +65,5 @@ df['soundSpel'] = cocaWordSS
 df['cocaRoot']  = cocaRoot
 df['cocaFreqPerMil']  = cocaFreqPerMil
 df.to_csv('workdir/coca_ordered_soundSpel_20k.csv', index=False)
+# grep NOT --color=never coca_ordered_soundSpel_20k.csv> not_in_soundSpel.csv
 
