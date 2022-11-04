34bcd6804feae017b78fdd1f5c2df4ec21f9aa5e
Mark Petersen <mpetersen@lanl.gov>
2022-11-04 09:03:21 -0600

    remove identical lines from twoColumns

--- dictionaries/soundSpel/tools/twoColumns.py
+++ dictionaries/soundSpel/tools/twoColumns.py
@@ -8,6 +8,7 @@ iLine=1
 
 # remove header line
 lineIn = fileIn.readline().rstrip('\n')
+prevField = ''
 while(True):
     #read next lineIn
     lineIn = fileIn.readline().rstrip('\n')
@@ -21,11 +22,14 @@ while(True):
 # revised:
 # 0TS,1soundSpel,2note,8Rondthaler,9pronunciation,7long note,3alternatives,4British,5multiple TS,6multiple SS
     field_list = lineIn.split(',')    # split the line at commas
-    lineOut = ','.join((field_list[0],   # rejoin with commas, new order
-                        field_list[1]
-                           ))+'\n'
-    # write out line
-    fileOut.write(lineOut)
+    # Only add if different from previous, to remove copies.
+    if field_list != prevField:
+        lineOut = ','.join((field_list[0],   # rejoin with commas, new order
+                            field_list[1]
+                               ))+'\n'
+        # write out line
+        fileOut.write(lineOut)
+    prevField = field_list
 
 #close file
 fileIn.close
